aa80fff4a39602b8d296d34a41e448ca5bdd8493
Oportugall <lemgruberportugalleonardo@gmail.com>
2026-04-15 17:01:09 -0300

    Funções 2

--- Guanabara python/Mundo 03/Aulas Mundo 03/funcões2.py
+++ Guanabara python/Mundo 03/Aulas Mundo 03/funcões2.py
@@ -1,1 +1,41 @@
-#help()
+"""help(input)
+print(input.__doc__)"""
+
+def contador(i, f, p):
+    c = i
+    while c <= f:
+        print(f'{c}', end = ' - ')
+        c += p
+    print('FIM!')
+
+contador(2, 10, 2)
+
+def somar(a, b, c = 0):
+    s = a + b + c
+    print(f'A soma vale {s}')
+
+somar(3, 4, 8)
+somar(4, 6)
+
+def teste(b):
+    global a
+    a = 8
+    b += 4
+    c = 2
+    print(f'A dentro vale {a}')
+    print(f'B dentro vale {b}')
+    print(f'C dentro vale {c}')
+
+a = 5
+teste(a)
+print(f'A fora vale {a}')
+
+def soma(a = 0, b = 0, c = 0):
+    s = a + b + c
+    return s
+
+r1 = soma(3, 2, 5)
+r2 = soma(1, 7)
+r3 = soma(4, 8)
+
+print(f'Os resultados foram {r1}, {r2} e {r3}')
---
+++ litcode/ex01.py
@@ -0,0 +1,9 @@
+nums = [2, 7, 11, 15]
+target = 9
+
+for i in range(len(nums)):
+    print(i)
+    """for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+            return [i, j]
+"""
