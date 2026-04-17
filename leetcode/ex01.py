efc7965908391a389205be2ee4b0011c53119cc5
Oportugall <lemgruberportugalleonardo@gmail.com>
2026-04-17 19:24:21 -0300

    Leet code 01

--- leetcode/ex01.py
+++ leetcode/ex01.py
@@ -3,7 +3,6 @@ target = 9
 
 for i in range(len(nums)):
     print(i)
-    """for j in range(i + 1, len(nums)):
+    for j in range(i + 1, len(nums)):
         if nums[i] + nums[j] == target:
             return [i, j]
-"""
