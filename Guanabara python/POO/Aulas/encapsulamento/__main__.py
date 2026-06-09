cf49d7072ad65dd56d2ff9db7cabbbd15879a34e
Oportugall <lemgruberportugalleonardo@gmail.com>
2026-06-09 14:05:51 -0300

    >>>

--- Guanabara python/POO/Aulas/encapsulamento/__main__.py
+++ Guanabara python/POO/Aulas/encapsulamento/__main__.py
@@ -1,11 +1,13 @@
 from classes import ContaBancaria
+from rich import inspect
 
 def main():
     c1 = ContaBancaria(111, 'Maria', 5_000)
     c1.depositar(-500)
     c1.sacar(-100)
-    c1.saldo = 00
+    c1.saldo = 0
     print(c1)
+    inspect(c1, private = True)
 
 if __name__ == '__main__':
     main()
